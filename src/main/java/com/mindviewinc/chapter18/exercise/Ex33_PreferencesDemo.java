4eb3cffd019bf0df8d582a89e6a88ab5b7dfe70e
BobbyTang <bobby3239@gmail.com>
2015-04-10 07:13:10 +0800

    minor update

--- src/main/java/com/mindviewinc/chapter18/exercise/Ex33_PreferencesDemo.java
+++ src/main/java/com/mindviewinc/chapter18/exercise/Ex33_PreferencesDemo.java
@@ -1,20 +1,13 @@
 package com.mindviewinc.chapter18.exercise;
 
-import java.util.Scanner;
-import java.util.prefs.Preferences;
+import java.util.Calendar;
 
 public class Ex33_PreferencesDemo {
 	public static void main(String[] args) throws Exception {
-		Preferences prefs = Preferences.userNodeForPackage(Ex33_PreferencesDemo.class);
-		String directory = prefs.get("base directory", "Not defined");
-		System.out.println("directory: " + directory);
-		// simple usage reading data from console input
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter a new directory: ");
-		//
-		directory = sc.nextLine();
-		prefs.put("base directory", directory);
-		System.out.println("\ndirectory: " + directory);
+		double d = -27.2345;
+		System.out.println(Math.ceil(d));
+		System.out.println(Math.round(d));
+		System.out.println(Math.abs(d));
+		System.out.println(Math.floor(d));
 	}
 }
